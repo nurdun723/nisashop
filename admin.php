468145a95895e855c4000cfcf42ff947d6c55fb9
nurdun723 <2574842004@qq.com>
2018-01-05 00:48:55 +0800

    20180105

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+//绑定模块
+define("BIND_MODULE","Admin");
+//模块路径
+define("APP_PATH","application/");
+//开启调试模式
+define("APP_DEBUG",true);
+
+//引入Think php的入口文件
+require_once 'library/ThinkPHP/ThinkPHP.php';
